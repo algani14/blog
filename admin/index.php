61e17663ce1e6606d061c6399fe1b8c5a911f5dc
algani14 <alghani837@gmal.com>
2019-11-24 23:38:07 -0500

    blog

---
+++ admin/index.php
@@ -0,0 +1,37 @@
+<!--Header-->
+<?php
+include('../layouts/includes/head.php');
+?>
+<!--end header-->
+  <body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
+      <!--navbar-->
+      <?php
+      include('../layouts/includes/navbar.php');
+      ?>
+      <!--end navbar-->
+    <div class="app-body">
+    <!--sidebar-->
+    <?php
+    include('../layouts/includes/sidebar.php');
+    ?>
+    <!--end sidebar-->
+      
+      <!--main-->
+      <main class="main">Ig:Algani14</main>
+      <!--end main-->
+      
+    </div>
+    <!--footer-->
+    <?php
+    include('../layouts/includes/footer.php');
+    ?>
+    <!--end footer-->
+    
+    <!-- CoreUI and necessary plugins-->
+    <!--scripts-->
+    <?php
+    include('../layouts/includes/scripts.php');
+    ?>
+    <!--end scripts-->
+    
+</html>
---
+++ layouts/includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="app-footer">
+      <div>
+        <a href="https://coreui.io">Blog</a>
+        <span>&copy; SMK Assalaam</span>
+      </div>
+      <div class="ml-auto">
+        <span></span>
+        <a href="https://coreui.io">SMK Assalaam</a>
+      </div>
+    </footer>
